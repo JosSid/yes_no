8c461b2b0bab4173a9e0c1204f425c0343412a71
Josep Valls <alfredvays@gmail.com>
2023-10-09 17:32:56 +0200

    ChatProvider.sendMessage

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -3,12 +3,16 @@ import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
 
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: 'Hola Amor!', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async {
-    //TODO implementar metodo
+    if(text.isEmpty) return;
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messageList.add(newMessage);
+
+    notifyListeners();
   }
 }
